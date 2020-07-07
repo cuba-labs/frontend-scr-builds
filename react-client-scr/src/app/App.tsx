929dc430b0e058aa50c31f1ee3ea31117017b0f2
Travis CI <>
2020-07-07 21:04:11 +0000

    Build 1066

--- react-client-scr/src/app/App.tsx
+++ react-client-scr/src/app/App.tsx
@@ -51,7 +51,7 @@ class AppComponent extends React.Component<
         <Layout.Header>
           <AppHeader />
         </Layout.Header>
-        <Layout className="layout-container">
+        <Layout>
           <Layout.Sider
             width={200}
             breakpoint="sm"
