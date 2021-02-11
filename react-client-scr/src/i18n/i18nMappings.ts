7a5c3bc34185f0c84d489fbf7d5889bcface8310
Travis CI <>
2021-02-11 10:15:53 +0000

    Build 1269

--- react-client-scr/src/i18n/i18nMappings.ts
+++ react-client-scr/src/i18n/i18nMappings.ts
@@ -4,15 +4,19 @@ import fr from "./fr.json";
 import frFr from "antd/es/locale/fr_FR";
 import ru from "./ru.json";
 import ruRu from "antd/es/locale/ru_RU";
+import zh from "./zh-cn.json";
+import zhCn from "antd/es/locale/zh_CN";
 
 export const antdLocaleMapping = {
   en: enUs,
   fr: frFr,
-  ru: ruRu
+  ru: ruRu,
+  "zh-cn": zhCn
 };
 
 export const messagesMapping = {
   en: en,
   fr: fr,
-  ru: ru
+  ru: ru,
+  "zh-cn": zh
 };
